1a05d8663049e875d808bd76671fe314c46e1b2a
thewanionly <wani.pitogo@gmail.com>
2022-07-15 06:23:12 +0800

    Prevent body from scrolling when menu is opened

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const body = document.querySelector('body')
 const navToggle = document.querySelector('#nav-toggle')
 const nav = document.querySelector('#nav')
 const footerCopyright = document.querySelector('#footer-copyright')
 
 navToggle.addEventListener('click', () => {
+  if (body?.style?.overflow === 'hidden') {
+    body.style.overflow = 'unset'
+  } else {
+    body.style.overflow = 'hidden'
+  }
+
   nav.classList.toggle('nav__menu')
   navToggle.classList.toggle('nav__toggle--close')
 })

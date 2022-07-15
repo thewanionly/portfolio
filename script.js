777aabe087960e3cea1dc752f220d7d20a6e7ce4
thewanionly <wani.pitogo@gmail.com>
2022-07-16 02:05:09 +0800

    Working nav links

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const body = document.querySelector('body')
 const navToggle = document.querySelector('#nav-toggle')
 const nav = document.querySelector('#nav')
+const navLinks = document.querySelectorAll('#nav-link')
 const footerCopyright = document.querySelector('#footer-copyright')
 
-navToggle.addEventListener('click', () => {
+// Nav toggle click handler
+const toggleNavMenu = () => {
   if (body?.style?.overflow === 'hidden') {
     body.style.overflow = 'unset'
   } else {
@@ -12,8 +14,24 @@ navToggle.addEventListener('click', () => {
 
   nav.classList.toggle('nav__menu')
   navToggle.classList.toggle('nav__toggle--close')
+}
+
+// Open/Close nav menu when nav toggle is clicked
+navToggle.addEventListener('click', toggleNavMenu)
+
+// Close nav menu and reset body overflow when clicking the nav link
+navLinks.forEach((link) => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    toggleNavMenu()
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    })
+  })
 })
 
+// Footer copyright
 if (footerCopyright) {
   footerCopyright.innerText = footerCopyright.innerText.replace('{year}', new Date().getFullYear())
 }

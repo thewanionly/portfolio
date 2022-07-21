501e2e175261792b07c9b78be8f0ed1e49068143
thewanionly <wani.pitogo@gmail.com>
2022-07-22 06:30:57 +0800

    Implement toast component. Show toast after successful/error sending of message

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const nav = document.querySelector('#nav')
 const navLinks = document.querySelectorAll('#nav-link')
 const footerCopyright = document.querySelector('#footer-copyright')
 const contactForm = document.querySelector('#contact-form')
+const toast = document.querySelector('#toast')
+const toastMesssage = document.querySelector('#toast-message')
+const toastClose = document.querySelector('#toast-close')
 
 // Nav toggle click handler
 const toggleNavMenu = () => {
@@ -53,32 +56,45 @@ contactForm.addEventListener('submit', async (e) => {
         Accept: 'application/json'
       }
     })
+    const resData = await res.json()
 
     // After request was done
+    toast.classList.add('toast--visible')
     submitButton.textContent = 'Send message'
     submitButton.disabled = false
 
     if (res.ok) {
       // Success
-
-      alert(
-        'Your message have been sent. Thank you for your interest. I will get back to you as soon as possible'
-      )
+      toast.classList.add('toast--success')
+      toastMesssage.textContent =
+        'Your message have been sent. I will get back to you as soon as possible. Thank you.'
       contactForm.reset()
     } else {
-      res.json().then((data) => {
-        if (Object.hasOwn(data, 'errors')) {
-          throw new Error(data['errors'].map((error) => error['message']).join(', '))
-        } else {
-          throw new Error('Oops! There was a problem submitting your form')
-        }
-      })
+      if (Object.hasOwn(resData, 'errors')) {
+        throw new Error(resData['errors'].map((error) => error['message']).join(', '))
+      } else if (Object.hasOwn(resData, 'error')) {
+        throw new Error(resData.error)
+      } else {
+        throw new Error('')
+      }
     }
   } catch (error) {
     //Error
-    alert(error)
+    let errorMessage = `Problem sending message.`
+
+    if (error?.message) {
+      errorMessage += ` ${error?.message}`
+    }
+
+    toast.classList.add('toast--error')
+    toastMesssage.textContent = errorMessage
     console.error(error)
   }
+})
+
+// Toast close
+toastClose.addEventListener('click', () => {
+  toast.classList.remove('toast--visible')
 })
 
 // Footer copyright
